fcc9830eda6d6a79bd0e7895d0a6057224e7d27d
Ajinkya <ajinkyasurvep@gmail.com>
2023-02-04 17:49:55 +0530

    Added differenct code for branch b1

--- Sample.py
+++ Sample.py
@@ -3,4 +3,8 @@
 n = input("Enter your name :")
 print(n)
 
-print("Hello ! My name is", n)
+n1 = int(input("Enter any number :"))
+if n1 % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
